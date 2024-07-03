6329ff411775cd5ba6b159b61bd38856964547d4
Ajmal Fariz <ajfariz13@gmail.com>
2024-07-03 13:37:08 +0530

    suspense

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react";
+import { Suspense, type FC } from "react";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -6,9 +6,11 @@ interface AuthLayoutProps {
 
 const AuthLayout: FC<Readonly<AuthLayoutProps>> = ({ children }) => {
   return (
+    <Suspense>
     <div className="min-h-screen flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       {children}
     </div>
+    </Suspense>
   );
 };
 
