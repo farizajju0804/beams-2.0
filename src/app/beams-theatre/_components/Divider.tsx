582cbe9dd8a9932074afec0e4f3b3ad40e0bc1af
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-03 21:50:29 +0530

    ui for theatre

--- src/app/beams-theatre/_components/Divider.tsx
+++ src/app/beams-theatre/_components/Divider.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Divider = () => {
   return (
     <div className='w-full flex items-center justify-center'>
-        <Image src="/images/beams-theatre/divider.png" alt='divider' width={600}  height={30}/>
+        <Image src="https://res.cloudinary.com/drlyyxqh9/image/upload/v1722701418/Beams%20Theatre/Divider_lzyjw0.png" alt='divider' width={600}  height={30}/>
     </div>
   )
 }
