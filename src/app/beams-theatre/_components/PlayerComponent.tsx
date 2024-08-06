a208ba91443bacdf542262ed6f42f597f8e7fdd5
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-06 22:14:20 +0530

    video player configuration

--- src/app/beams-theatre/_components/PlayerComponent.tsx
+++ src/app/beams-theatre/_components/PlayerComponent.tsx
@@ -17,7 +17,7 @@ const PlayerComponent = () => {
           ]}
         />
 
-     <VideoPlayer  id={'1234'} videoId="https://res.cloudinary.com/drlyyxqh9/video/upload/v1722598269/Beams%20today/Medical%20Miracle/medical-miracle-video_lyqh23.mp4" thumbnailUrl="https://res.cloudinary.com/drlyyxqh9/image/upload/v1722598239/Beams%20today/Medical%20Miracle/Medical_Miracle_Thumbnail_iky88e.png" />
+     <VideoPlayer videoTitle='Medical Miracle' id={'1234'} videoId="https://res.cloudinary.com/drlyyxqh9/video/upload/v1722598269/Beams%20today/Medical%20Miracle/medical-miracle-video_lyqh23.mp4" thumbnailUrl="https://res.cloudinary.com/drlyyxqh9/image/upload/v1722598239/Beams%20today/Medical%20Miracle/Medical_Miracle_Thumbnail_iky88e.png" />
       <EpisodeDetails/>
         
         </div>
