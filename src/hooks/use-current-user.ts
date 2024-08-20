cae27a856bdbb82a6ac028b013afe8f4e893d9da
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-20 14:33:57 +0530

    maximum depth bug fix

--- src/hooks/use-current-user.ts
+++ src/hooks/use-current-user.ts
@@ -21,29 +21,17 @@ import { useEffect } from "react";
 import { useUserStore } from "@/store/userStore";
 
 export const useCurrentUser = () => {
-  const { data: session, status, update } = useSession(); 
+  const { data: session, status } = useSession(); 
   const setUser = useUserStore((state) => state.setUser);
   const user = useUserStore((state) => state.user);
 
   useEffect(() => {
-    const updateAndStoreUser = async () => {
-      if (status === "authenticated" && session?.user) {
-        // Update the session data if necessary
-        const updatedSession:any = await update({
-          ...session.user,
-          // email: session.user.email ?? '',
-          // firstName: session.user.firstName ?? '',
-          // lastName: session.user.lastName ?? '',
-          // Add any other fields you want to update
-        });
-
-        // Store the updated session data in Zustand
-        setUser(updatedSession?.user || session.user);
-      }
-    };
-
-    updateAndStoreUser();
-  }, [session, status, setUser, update]);
+    if (status === "authenticated" && session?.user) {
+      // Store the session data in Zustand only if authenticated
+      setUser(session.user);
+    }
+  }, [session, status, setUser]);
 
   return user;
 };
+
