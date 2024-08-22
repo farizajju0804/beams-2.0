3aa2f80b3fb383d88a5b0c9f7cb15c1823ec66a6
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-22 13:27:30 +0530

    ip tracking and redirection

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -7,7 +7,7 @@ interface AuthLayoutProps {
 const AuthLayout: FC<Readonly<AuthLayoutProps>> = ({ children }) => {
   return (
     <Suspense>
-    <div className="min-h-screen w-full flex items-center justify-center bg-yellow"
+    <div className="min-h-screen w-full max-w-full flex items-center justify-center bg-yellow overflow-x-hidden"
     style={{
       backgroundImage: `url(${'https://img.freepik.com/free-vector/stylish-bright-yellow-color-halftone-background_1055-8924.jpg?t=st=1723291082~exp=1723294682~hmac=76fbdcbee564fde2815c5a090e576aa1b47106892a9f9c8c47a4c69aa352b6ae&w=826'})`,
       backgroundSize: "cover",
