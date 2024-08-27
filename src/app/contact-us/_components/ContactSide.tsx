c7d62782972e37b776935ad36881110bc468e5b4
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-27 19:56:43 +0530

    contact apos fix

--- src/app/contact-us/_components/ContactSide.tsx
+++ src/app/contact-us/_components/ContactSide.tsx
@@ -8,7 +8,7 @@ const ContactSide: React.FC = () => {
 
       className='bg-yellow flex flex-col items-center justify-center p-8 lg:rounded-l-2xl flex-1'
     >
-      <h2 className='text-xl md:text-3xl text-left w-full font-poppins font-bold mb-2 md:mb-4'>Let's Have a Talk</h2>
+      <h2 className='text-xl md:text-3xl text-left w-full font-poppins font-bold mb-2 md:mb-4'>Let&apos;s Have a Talk</h2>
       <p className='mb-4 text-sm'>Whether you have questions, need assistance, or just want to provide feedback, our team is ready to assist you.</p>
       <div className='flex items-center justify-center'>
       <Image priority src={'/images/contact.png'} alt='san francisco' className='m-auto' width={270} height={340} />
