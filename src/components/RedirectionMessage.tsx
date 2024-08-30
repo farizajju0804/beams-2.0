f9aa3ab8cdce939973b9cfdeaed09340500bcd08
Ajmal Fariz <ajfariz13@gmail.com>
2024-08-30 15:31:37 +0530

    redirection messages added

--- src/components/RedirectionMessage.tsx
+++ src/components/RedirectionMessage.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const RedirectionMessage = () => {
   return (
    
-        <div className="fixed min-h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-md shadow-lg text-center">
+        <div className="fixed min-h-screen inset-0 bg-text bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-background p-6 rounded-md shadow-lg text-center">
             <p className="text-lg font-poppins text-green-500 font-semibold mb-2">Submission Successful!</p>
-            <p>Redirecting ...</p>
+            <p className='text-text'>Redirecting ...</p>
           </div>
         </div>
       
