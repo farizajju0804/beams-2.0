bf94aa19efae07c1c5e60f73d9e62887e8c661e3
farizajju0804 <ajfariz13@gmail.com>
2024-09-20 19:35:36 +0530

    leaderboard winner announce final check

--- src/app/dashboard/_components/Leaderboard.tsx
+++ src/app/dashboard/_components/Leaderboard.tsx
@@ -52,7 +52,7 @@ const CustomModal = ({ isOpen, onClose, children, message }: any) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
       <div className="bg-background p-8 rounded-lg max-w-md w-full">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-bold">Last Week's Top 3!</h2>
+          <h2 className="text-2xl font-bold">Last Week&apos;s Top 3!</h2>
           <button onClick={onClose} className="text-text">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
